3e6e84b5b39ac47780ea3ecf947f55343219dab6
Ben Pirt <ben@pirt.co.uk>
2016-02-26 15:02:26 +0000

    Bump the library version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'mirobot',
   packages = ['mirobot'],
-  version = '1.0.1',
+  version = '1.0.2',
   description = 'A Python library to control Mirobot (http://mirobot.io)',
   author = 'Ben Pirt',
   author_email = 'ben@pirt.co.uk',
   url = 'https://github.com/mirobot/mirobot-py',
-  download_url = 'https://github.com/mirobot/mirobot-py/tarball/v1.0.1',
+  download_url = 'https://github.com/mirobot/mirobot-py/tarball/v1.0.2',
   keywords = ['robotics', 'control', 'mirobot'],
   classifiers = ['Programming Language :: Python :: 2', 'Programming Language :: Python :: 3', 'Topic :: Education', 'License :: OSI Approved :: MIT License'],
 )
