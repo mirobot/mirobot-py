c745ec309c3911eeb67bf961dcd5205addd83db1
Ben Pirt <ben@pirt.co.uk>
2016-04-12 10:49:10 +0100

    Update the package information

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'mirobot',
   packages = ['mirobot'],
-  version = '1.0.2',
+  version = '1.0.3',
   description = 'A Python library to control Mirobot (http://mirobot.io)',
   author = 'Ben Pirt',
   author_email = 'ben@pirt.co.uk',
@@ -10,4 +10,7 @@ setup(
   download_url = 'https://github.com/mirobot/mirobot-py/tarball/v1.0.2',
   keywords = ['robotics', 'control', 'mirobot'],
   classifiers = ['Programming Language :: Python :: 2', 'Programming Language :: Python :: 3', 'Topic :: Education', 'License :: OSI Approved :: MIT License'],
+  install_requires=[
+    "websocket-client",
+  ],
 )
